85a420bd1264b4b4871a5b3878e2e30e489796c5
Christiana <Christiana@Christianas-MacBook-Air.local>
2026-02-04 15:40:42 -0700

    set up done

---
+++ server.js
@@ -0,0 +1,25 @@
+// Imports
+import express from "express";
+import dotenv from "dotenv";
+import { logReq, globalErr } from "./middleware/middlewares.js";
+
+// Setups
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(logReq);
+
+// Route
+app.use("api/", );
+
+// Global Err
+app.use(globalErr);
+
+// Listener
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT: ${PORT}`);
+});
