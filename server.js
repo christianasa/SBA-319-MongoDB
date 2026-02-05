415437c4274f89c2ed2a5f6cf6b96588b6a90cc6
Christiana <Christiana@Christianas-MacBook-Air.local>
2026-02-05 09:48:01 -0700

    sever update

--- server.js
+++ server.js
@@ -2,6 +2,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import { logReq, globalErr } from "./middleware/middlewares.js";
+import connectDB from "./db/conn.js";
+import brandRoutes from "./routes/brandRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
 
 // Setups
 dotenv.config();
@@ -9,17 +13,19 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 connectDB();
 
-// Middleware
+// Middlewares
 app.use(express.json());
 app.use(logReq);
 
-// Route
-app.use("api/", );
+// Routes
+app.use("/api/brands", brandRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/reviews", reviewRoutes);
 
-// Global Err
+// Global Error Handling
 app.use(globalErr);
 
 // Listener
 app.listen(PORT, () => {
-  console.log(`Server Listening on PORT: ${PORT}`);
+  console.log(`Server listening on PORT: ${PORT}`);
 });
